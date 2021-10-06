cda8d4d45e6dbf72572fc0c76ce7ded147816e58
202-14\User <frogman@gmail.com>
2021-10-06 15:12:29 +0900

    [최원제]  -열공필템 쇼핑몰 최초 시작

---
+++ src/test/java/com/Yeolgongpiltem/YeolgongpiltemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Yeolgongpiltem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeolgongpiltemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
