0499f4a90f5837f89a660ccfa43c5df35554fd73
DESKTOP-4GKAT56\JE <circleash92@gmail.com>
2021-10-12 22:28:04 +0900

    [최원제]  - 열공필템 기초 틀 잡기

---
+++ src/main/java/com/Yeolgongpiltem/main/MainController.java
@@ -0,0 +1,14 @@
+package com.Yeolgongpiltem.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+	@GetMapping("/main_view")
+	public String mainView() {
+		return "main/main";
+	}
+}
---
+++ src/main/java/com/Yeolgongpiltem/user/UserRestController.java
@@ -0,0 +1,5 @@
+package com.Yeolgongpiltem.user;
+
+public class UserRestController {
+
+}
